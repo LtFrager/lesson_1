df04df2c8837740dca79f410bdb5474e76fa60be
LtFrager <sadesade2011@gmail.com>
2019-04-26 23:28:38 +0300

    company_blue_block

--- zakaz/js/js.js
+++ zakaz/js/js.js
@@ -16,4 +16,58 @@ $(document).ready(function($) {
         autoplaySpeed: 2000,
     });
 
+    $('.mb1').hover(function() {
+        $('.block1').css({
+            opacity: '1',
+        });
+    }, function() {
+        $('.block1').css({
+            opacity: '0',
+        });
+    });
+    $('.mb2').hover(function() {
+        $('.block2').css({
+            opacity: '1',
+        });
+    }, function() {
+        $('.block2').css({
+            opacity: '0',
+        });
+    });
+    $('.mb3').hover(function() {
+        $('.block3').css({
+            opacity: '1',
+        });
+    }, function() {
+        $('.block3').css({
+            opacity: '0',
+        });
+    });
+    $('.mb4').hover(function() {
+        $('.block4').css({
+            opacity: '1',
+        });
+    }, function() {
+        $('.block4').css({
+            opacity: '0',
+        });
+    });
+    $('.mb5').hover(function() {
+        $('.block5').css({
+            opacity: '1',
+        });
+    }, function() {
+        $('.block5').css({
+            opacity: '0',
+        });
+    });
+    $('.mb6').hover(function() {
+        $('.block6').css({
+            opacity: '1',
+        });
+    }, function() {
+        $('.block6').css({
+            opacity: '0',
+        });
+    });
 });
