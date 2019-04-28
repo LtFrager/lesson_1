8d3016615cb2e35b0c86761febc63d110985c5b8
LtFrager <sadesade2011@gmail.com>
2019-04-28 15:50:46 +0300

    fix

--- zakaz/js/js.js
+++ zakaz/js/js.js
@@ -46,7 +46,7 @@ $(document).ready(function($) {
         }]
     });
 
-    $('.lightgallery').lightGallery();
+
 
     $(function() {
         $(".clickable-active").click(function() {
@@ -55,58 +55,73 @@ $(document).ready(function($) {
         });
     });
 
-    $('.mb1').hover(function() {
-        $('.block1').css({
+    var id = 0;
+    $('.mb').hover(function() {
+        id = $(this).data('id');
+        console.log(id)
+        $('.block' + id).css({
             opacity: '1',
         });
     }, function() {
-        $('.block1').css({
+        $('.block' + id).css({
             opacity: '0',
         });
     });
-    $('.mb2').hover(function() {
-        $('.block2').css({
-            opacity: '1',
-        });
-    }, function() {
-        $('.block2').css({
-            opacity: '0',
-        });
-    });
-    $('.mb3').hover(function() {
-        $('.block3').css({
-            opacity: '1',
-        });
-    }, function() {
-        $('.block3').css({
-            opacity: '0',
-        });
-    });
-    $('.mb4').hover(function() {
-        $('.block4').css({
-            opacity: '1',
-        });
-    }, function() {
-        $('.block4').css({
-            opacity: '0',
-        });
-    });
-    $('.mb5').hover(function() {
-        $('.block5').css({
-            opacity: '1',
-        });
-    }, function() {
-        $('.block5').css({
-            opacity: '0',
-        });
-    });
-    $('.mb6').hover(function() {
-        $('.block6').css({
-            opacity: '1',
-        });
-    }, function() {
-        $('.block6').css({
-            opacity: '0',
-        });
-    });
+
+    // $('.mb1').hover(function() {
+    //     $('.block1').css({
+    //         opacity: '1',
+    //     });
+    // }, function() {
+    //     $('.block1').css({
+    //         opacity: '0',
+    //     });
+    // });
+    // $('.mb2').hover(function() {
+    //     $('.block2').css({
+    //         opacity: '1',
+    //     });
+    // }, function() {
+    //     $('.block2').css({
+    //         opacity: '0',
+    //     });
+    // });
+    // $('.mb3').hover(function() {
+    //     $('.block3').css({
+    //         opacity: '1',
+    //     });
+    // }, function() {
+    //     $('.block3').css({
+    //         opacity: '0',
+    //     });
+    // });
+    // $('.mb4').hover(function() {
+    //     $('.block4').css({
+    //         opacity: '1',
+    //     });
+    // }, function() {
+    //     $('.block4').css({
+    //         opacity: '0',
+    //     });
+    // });
+    // $('.mb5').hover(function() {
+    //     $('.block5').css({
+    //         opacity: '1',
+    //     });
+    // }, function() {
+    //     $('.block5').css({
+    //         opacity: '0',
+    //     });
+    // });
+    // $('.mb6').hover(function() {
+    //     $('.block6').css({
+    //         opacity: '1',
+    //     });
+    // }, function() {
+    //     $('.block6').css({
+    //         opacity: '0',
+    //     });
+    // });
+
+    $('.lightgallery').lightGallery();
 });
