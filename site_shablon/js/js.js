e58e6bd5965eefe88dd2f8db1554061583772ba5
LtFrager <sadesade2011@gmail.com>
2019-05-11 21:22:19 +0300

    hw-7

--- site_shablon/js/js.js
+++ site_shablon/js/js.js
@@ -1,5 +1,35 @@
 // alert('test - JavaScript!');
-// $(document).ready(function() {
-//     console.log("ready!");
-//     alert('test - JQuery');
-// });
+$(document).ready(function() {
+    $(function() {
+        $("#NE").click(function() {
+            $('.style-change').removeClass("ne-style human-style orc-style undead-style");
+            $('.style-change').addClass("ne-style");
+            $('.style-change-btn').removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+    $(function() {
+        $("#Human").click(function() {
+            $('.style-change').removeClass("ne-style human-style orc-style undead-style");
+            $('.style-change').addClass("human-style");
+            $('.style-change-btn').removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+    $(function() {
+        $("#Orc").click(function() {
+            $('.style-change').removeClass("ne-style human-style orc-style undead-style");
+            $('.style-change').addClass("orc-style");
+            $('.style-change-btn').removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+    $(function() {
+        $("#Undead").click(function() {
+            $('.style-change').removeClass("ne-style Human-style orc-style undead-style");
+            $('.style-change').addClass("undead-style");
+            $('.style-change-btn').removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+});
