39d51c54939ec04641cd5f7e05741655565c6cc4
LtFrager <sadesade2011@gmail.com>
2019-06-18 13:25:55 +0300

    l16

---
+++ home_works/lesson-16-home-work/js/js.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        draggable: false,
+    });
+
+    $('a[data-slide]').click(function(e) {
+        e.preventDefault();
+        var slideno = $(this).data('slide');
+        $('.slider-nav').slick('slickGoTo', slideno - 1);
+        $('.tab-nav-item a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    $('.slider-clients').slick({
+        slidesToShow: 7,
+        slidesToScroll: 3,
+        dots: false,
+        arrows: false,
+        variableWidth: true,
+        // infinite: true,
+        autoplay: true,
+        // draggable: false,
+
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $(".my-open").click(function() {
+        $(".nav-list").addClass('opened');
+        $(".my-open").css("opacity", "0");
+    });
+
+
+    $(".my-close").click(function() {
+        $(".nav-list").removeClass('opened');
+        $(".my-open").css("opacity", "1");
+    });
+});
